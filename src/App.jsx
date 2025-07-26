600a089a1685699fec8075a93391e28a62143627
M3NGSZE <mengse.thun13@gmail.com>
2025-07-26 11:31:07 +0700

    after a long ass config tail and flowbite component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import NavbarComponent from './components/NavbarComponent'
 
 function App() {
-  
-
   return (
     <>
-      <h1>Hello world</h1>
+      <NavbarComponent />
+      <div className="bg-blue-500 text-white p-4 m-4 rounded-lg">
+        <h1 className="text-2xl font-bold">Hello world</h1>
+        <p className="text-sm">This should be styled with Tailwind CSS</p>
+      </div>
     </>
   )
 }
